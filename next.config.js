8b57c639c4668f30669b887974bbeb3dd518a4a7
0xFaded <xav@bracket.software>
2024-03-06 13:34:56 +0000

    Fix assetPrefix path in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: "cookly-presale.github.io",
+    assetPrefix: "/cookly-presale.github.io",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
